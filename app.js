461192ab59bca1351078fcdd54b8d8d09960f895
Cherifi-Houdaifa <houdaifacherifi618@gmail.com>
2022-04-17 17:18:48 -0700

    Added functionality to the game UI

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
+// game variables
 const plays = ["rock", "paper", "scissors"];
+let result = "";
+let playerSelection = "";
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let result = ""
-    let playerSelection = "";
+// DOM variables
+const btns = document.querySelectorAll("input");
+const refresh = document.querySelector(".winner input");
+let playerScore = document.querySelector(".player .score");
+let computerScore = document.querySelector(".computer .score");
+let roundStatus = document.querySelector(".round-status p");
+let winner = document.querySelector(".winner p");
 
-    for (let i = 0; i < 5; i++) {
-        while (true) {
-            playerSelection = prompt("Choose (rock), (paper), (scissors):")
-            if (plays.includes(playerSelection.toLowerCase())) {
-                break
-            }
-        }
-        result = playRound(playerSelection, computerPlay())
-        console.log(result)
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        result = playRound(btn.classList[0], computerPlay());
+        roundStatus.textContent = result;
+
         if (result.startsWith("You Win")) {
-            playerScore++
+            playerScore.textContent = Number(playerScore.textContent) + 1;
         }
         else if (result.startsWith("You Lose")) {
-            computerScore++
+            computerScore.textContent = Number(computerScore.textContent) + 1;
         }
-    }
-    if (playerScore > computerScore) {
-        console.log("You Win the game")
-    }
-    else {
-        console.log("You Lose the game")
-    }
-}
+        if (Number(playerScore.textContent) === 5) {
+            winner.textContent = "You win";
+            refresh.removeAttribute("hidden");
+        }
+        else if (Number(computerScore.textContent === 5)) {
+            winner.textContent = "You lose";
+            refresh.removeAttribute("hidden");
+        }
+    })
+})
+
 
 
 function computerPlay() {
@@ -53,4 +58,3 @@ function playRound(player, computer) {
         return `You Lose! ${computer} beats ${player}`
     }
 }
-game()
